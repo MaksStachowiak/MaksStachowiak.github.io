b0bc5762cf052fe8fe4f50fd7990916c43b5df87
MaksStachowiak <lustitaj@gmail.com>
2020-06-08 09:38:16 +0200

    asset fix

--- weather_boy/flutter_service_worker.js
+++ weather_boy/flutter_service_worker.js
@@ -4,7 +4,7 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "manifest.json": "d81a59ef4b3e11b2db49b344ba10a05c",
-"main.dart.js": "bd477f467662c4ffb3e7537cccce648f",
+"main.dart.js": "031f38cebc3a80b7be9455768d1e6db8",
 "index.html": "28f901d41567c627ee3668e6f93075d0",
 "/": "28f901d41567c627ee3668e6f93075d0",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
@@ -18,7 +18,22 @@ const RESOURCES = {
 "assets/assets/clouds.jpg": "e130c315c9ade37b27f5c1fcbbff44ff",
 "assets/assets/indicator.jpg": "501070451fc53660ea630401c29c6743",
 "assets/assets/overcast.jpg": "204f5c64d4e4e2404449ccce73558616",
-"assets/assets/pluie-fine.jpg": "14504795a7f04e69504ddf7b6576e5f5"
+"assets/assets/pluie-fine.jpg": "14504795a7f04e69504ddf7b6576e5f5",
+".git/config": "5b603c2c0801a9ded3b79159fc38b404",
+".git/hooks/prepare-commit-msg.sample": "2b5c047bdb474555e1787db32b2d2fc5",
+".git/hooks/pre-receive.sample": "2ad18ec82c20af7b5926ed9cea6aeedd",
+".git/hooks/pre-rebase.sample": "56e45f2bcbc8226d2b4200f7c46371bf",
+".git/hooks/update.sample": "517f14b9239689dff8bda3022ebd9004",
+".git/hooks/pre-push.sample": "3c5989301dd4b949dfa1f43738a22819",
+".git/hooks/post-update.sample": "2b7ea5cee3c49ff53d41e00785eb974c",
+".git/hooks/pre-applypatch.sample": "054f9ffb8bfe04a599751cc757226dda",
+".git/hooks/pre-commit.sample": "e4db8c12ee125a8a085907b757359ef0",
+".git/hooks/applypatch-msg.sample": "ce562e08d8098926a3862fc6e7905199",
+".git/hooks/fsmonitor-watchman.sample": "ecbb0cb5ffb7d773cd5b2407b210cc3b",
+".git/hooks/commit-msg.sample": "579a3c1e12a1e74a98169175fb913012",
+".git/info/exclude": "036208b4a1ab4a235d75c181e685e5a3",
+".git/description": "a0a7c3fff21f2aea3cfa1d0316dd816c",
+".git/HEAD": "4cf2d64e44205fe628ddd534e1151b58"
 };
 
 // The application shell files that are downloaded before a service worker can
